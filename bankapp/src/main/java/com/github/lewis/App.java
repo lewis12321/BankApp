98072a2b56aef01a56edfb7aa480ea492eac3d18
lewis12321 <lewis_jefferies@live.co.uk>
2017-05-11 17:41:59 +0100

    Create account completed
    now creates accounts

--- bankapp/src/main/java/com/github/lewis/App.java
+++ bankapp/src/main/java/com/github/lewis/App.java
@@ -1,11 +1,48 @@
 package com.github.lewis;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+
+    private static int accountNumber;
+
+    public static void main( String[] args ) throws IOException {
         System.out.println( "Hello, Welcome to the Bank." );
         System.out.println("Would you like to do?");
         System.out.println("1. Create account");
+
+        String[] account = createAccount();
+        saveAccount(args[0], account);
+    }
+
+    public static void saveAccount(String fileLocation, String[] account) throws IOException {
+        CSVWriter writer = new CSVWriter(new FileWriter(fileLocation));
+
+        writer.writeNext(account);
+        writer.flush();
+        writer.close();
+    }
+
+    private static String[] createAccount() {
+        String[] account = new String[3];
+        account[0] = Integer.toString(createAccountNumber());
+        account[1] = createPassword();
+        account[2] = "0";
+        return account;
+    }
+
+    public static int createAccountNumber(){
+        return accountNumber++;
+    }
+
+    public static String createPassword(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your desired password.");
+        return scanner.nextLine();
     }
 }
--- bankapp/src/test/java/com/github/lewis/AccountCreatorTest.java
+++ bankapp/src/test/java/com/github/lewis/AccountCreatorTest.java
@@ -13,13 +13,14 @@ public class AccountCreatorTest {
 
     @Test
     public void testCreateAccount() throws IOException {
-        CSVReader reader = new CSVReader(new FileReader("C:\\Users\\Fraser\\IdeaProjects\\BankApp\\Accounts\\accounts.csv"));
+        String[] accountCreator = new String[] {"40000000", "password", "0"};
+        App.saveAccount("C:\\Users\\Fraser\\IdeaProjects\\BankApp\\bankapp\\Accounts\\accounts.csv", accountCreator);
+
+        CSVReader reader = new CSVReader(new FileReader("C:\\Users\\Fraser\\IdeaProjects\\BankApp\\bankapp\\Accounts\\accounts.csv"));
         String[] account = reader.readNext();
         assertEquals(account[0], "40000000");
         assertEquals(account[1], "password");
         assertEquals(account[2], "0");
     }
-
-
 
 }
